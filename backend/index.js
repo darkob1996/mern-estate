6ab23cf966e9fdbb39f8b2a4ce0feae30448420a
Darko Boljanović <71256137+darkob1996@users.noreply.github.com>
2024-03-31 11:10:17 +0200

    database

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,25 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: "./config.env",
+});
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000!!");
+const DB = process.env.DATABASE_URL.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB)
+  .then(() => console.log("Connected to DB."))
+  .catch((err) => console.log(err));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log("Listening on port 3000.");
 });
